838aeb27b31539817ad1d404768d6b6deabcbdf2
RimmaOgulchanskaia used for github <rimmaogulchanskaya@gmail.com used for github>
2025-07-30 16:40:32 +0300

    NegativeTests login and add new contact

--- src/test/java/ui_mobile/EditContactTest.java
+++ src/test/java/ui_mobile/EditContactTest.java
@@ -2,12 +2,11 @@ package ui_mobile;
 
 import config.AppiumConfig;
 import dto.User;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import screens.AuthenticationScreen;
-import screens.ContactsScreen;
-import screens.EditContactScreen;
-import screens.SplashScreen;
+import screens.*;
+
 import static utils.ContactFactory.*;
 
 public class EditContactTest extends AppiumConfig {
@@ -32,4 +31,78 @@ public class EditContactTest extends AppiumConfig {
         contactsScreen.swipeLeftToRight()
         .editContact(createPositiveContact());
     }
+
+    @Test
+    public void editContactNegativeTest_WrongEmail(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongEmail("Rrr@"));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("email=must be a well-formed email address"));
+
+    }
+    @Test
+    public void editContactNegativeTest_WrongEmail2(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongEmail("@gmail.com"));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("email=must be a well-formed email address"));
+
+    }
+
+    @Test
+    public void editContactNegativeTest_WrongEmail3(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongEmail("Rimmagmail.com"));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("email=must be a well-formed email address"));
+
+    }
+
+    @Test
+    public void editContactNegativeTest_EmptyEmail(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongEmail(" "));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("email=must be a well-formed email address"));
+
+    }
+
+    @Test
+    public void editContactNegativeTest_WrongName(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongName(""));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("name=must not be blank, email=must be a well-formed email address"));
+
+    }
+
+    @Test
+    public void editContactNegativeTest_WrongLastName(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongLastName(""));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("lastName=must not be blank, email=must be a well-formed email address"));
+
+    }
+    @Test
+    public void editContactNegativeTest_WrongPhone(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongPhone(""));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("phone=Phone number must contain only digits! And length min 10, max 15!, email=must be a well-formed email address"));
+
+    }
+
+    @Test
+    public void editContactNegativeTest_WrongAddress(){
+        contactsScreen.swipeLeftToRight()
+                .editContact(createNegativeContact_WrongAddress(""));
+        Assert.assertTrue(new ErrorScreen(driver)
+                .validateErrorMessage("address=must not be blank, email=must be a well-formed email address"));
+
+    }
+
+
+
+
 }
